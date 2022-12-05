bab6fc793b37b4ec52a31ba7076f9e75161b1e42
Tarik Sahin <tarik.sahin@unibw.de>
2022-12-05 16:20:17 +0100

    Add more explanations, add fem solution

--- elasticity_2d/Hertzian_normal_contact.py
+++ elasticity_2d/Hertzian_normal_contact.py
@@ -1,11 +1,14 @@
 import deepxde as dde
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 import os
 import sys
 from pathlib import Path
 from deepxde.backend import tf
+import matplotlib.tri as tri
 from pyevtk.hl import unstructuredGridToVTK
+import time
 # add utils folder to the system path
 path_utils = str(Path(__file__).parent.parent.absolute()) + "/utils"
 sys.path.append(path_utils)
@@ -40,8 +43,8 @@ lame = 115.38461538461539
 shear = 76.92307692307692
 elasticity_utils.lame = lame
 elasticity_utils.shear = shear
-nu_analy,lame_analy,shear_analy,e_modul_analy = problem_parameters() # with dimensions, will be used for analytical solution
-# This will lead to e_modul_analy=200 and nu_analy=0.3
+nu,lame,shear,e_modul = problem_parameters() # with dimensions, will be used for analytical solution
+# This will lead to e_modul=200 and nu=0.3
 
 # The applied pressure 
 ext_traction = -0.5
@@ -51,67 +54,25 @@ elasticity_utils.geom = geom
 
 distance = 0
 
-def fun_gap(x, y, X):
-
+def calculate_gap_in_normal_direction(x,y,X):
+    '''
+    Calculates the gap in normal direction
+    '''
+    # calculate the gap in y direction    
     gap_y = x[:,1:2] + y[:,1:2] + radius + distance
 
+    # calculate the boundary normals
     normals, cond = calculate_boundary_normals(X,geom)
 
-    gn = tf.math.divide_no_nan(gap_y[cond],tf.math.abs(normals[:,1:2]))
-
-    return (1-tf.math.sign(gn))*gn
-
-def normal_traction(x,y,X):
+    # Here is the idea to calculate gap_n:
+    # gap_n/|n| = gap_y/|ny| --> since n is unit vector |n|=1
+    gap_n = tf.math.divide_no_nan(gap_y[cond],tf.math.abs(normals[:,1:2]))
     
-    sigma_xx, sigma_yy, sigma_xy = y[:, 2:3], y[:, 3:4], y[:, 4:5]
+    return gap_n
 
-    normals, cond = calculate_boundary_normals(X,geom)
-
-    sigma_xx_nx_nx = sigma_xx[cond]*normals[:,0:1]*normals[:,0:1]
-    sigma_xy_ny_nx = sigma_xy[cond]*normals[:,1:2]*normals[:,0:1]
-    sigma_yx_nx_ny = sigma_xy[cond]*normals[:,0:1]*normals[:,1:2]
-    sigma_yy_ny_ny = sigma_yy[cond]*normals[:,1:2]*normals[:,1:2]
-
-    Pn = sigma_xx_nx_nx+sigma_xy_ny_nx+sigma_yx_nx_ny+sigma_yy_ny_ny
-
-    return (1+tf.math.sign(Pn))*Pn
-
-def tangential_traction(x,y,X):
-    
-    sigma_xx, sigma_yy, sigma_xy = y[:, 2:3], y[:, 3:4], y[:, 4:5]
-
-    normals, cond = calculate_boundary_normals(X,geom)
-
-    sigma_xx_nx_ny = sigma_xx[cond]*normals[:,0:1]*normals[:,1:2]
-    sigma_xy_ny_ny = sigma_xy[cond]*normals[:,1:2]*normals[:,1:2]
-    sigma_yx_nx_nx = sigma_xy[cond]*normals[:,0:1]*normals[:,0:1]
-    sigma_yy_nx_ny = sigma_yy[cond]*normals[:,0:1]*normals[:,1:2]
-
-    Tt = -sigma_xx_nx_ny-sigma_xy_ny_ny+sigma_yx_nx_nx+sigma_yy_nx_ny
-
-    return Tt
-
-def complimentary(x,y,X):
-    
-    normals, cond = calculate_boundary_normals(X,geom)
-
-    gap_y = x[:,1:2] + y[:,1:2] + radius +  distance
-    gn = tf.math.divide_no_nan(gap_y[cond],tf.math.abs(normals[:,1:2]))
-
-    sigma_xx, sigma_yy, sigma_xy = y[:, 2:3], y[:, 3:4], y[:, 4:5]
-
-    sigma_xx_nx_nx = sigma_xx[cond]*normals[:,0:1]*normals[:,0:1]
-    sigma_xy_ny_nx = sigma_xy[cond]*normals[:,1:2]*normals[:,0:1]
-    sigma_yx_nx_ny = sigma_xy[cond]*normals[:,0:1]*normals[:,1:2]
-    sigma_yy_ny_ny = sigma_yy[cond]*normals[:,1:2]*normals[:,1:2]
-
-    Pn = sigma_xx_nx_nx+sigma_xy_ny_nx+sigma_yx_nx_ny+sigma_yy_ny_ny
-
-    return gn*Pn
-
-def zero_traction_x(x, y, X):
+def calculate_traction_x(x, y, X):
     '''
-    Represents the x component of the applied pressure
+    X component of any traction vector using by Cauchy stress tensor, general implementation
     '''
 
     sigma_xx, sigma_yy, sigma_xy = y[:, 2:3], y[:, 3:4], y[:, 4:5] 
@@ -125,9 +86,9 @@ def zero_traction_x(x, y, X):
 
     return traction_x
 
-def zero_traction_y(x, y, X):
+def calculate_traction_y(x, y, X):
     '''
-    Represents the x component of the applied pressure
+    Y component of any traction vector using by Cauchy stress tensor, general implementation
     '''
 
     sigma_xx, sigma_yy, sigma_xy = y[:, 2:3], y[:, 3:4], y[:, 4:5] 
@@ -141,6 +102,76 @@ def zero_traction_y(x, y, X):
 
     return traction_y
 
+def calculate_normal_traction(x,y,X):
+    '''
+    Calculates the normal component of traction vector.
+    '''
+    # traction vector has 2 components: Tx and Ty
+    Tx = calculate_traction_x(x, y, X)
+    Ty = calculate_traction_y(x, y, X)
+
+    normals, _ = calculate_boundary_normals(X,geom)
+    nx = normals[:,0:1]
+    ny = normals[:,1:2]
+
+    Pn = Tx*nx+ Ty*ny
+    
+    return Pn
+
+def calculate_tangential_traction(x,y,X):
+    '''
+    Calculates the tangential component of traction vector.
+    '''
+    
+    # traction vector has 2 components: Tx and Ty
+    Tx = calculate_traction_x(x, y, X)
+    Ty = calculate_traction_y(x, y, X)
+
+    normals, _ = calculate_boundary_normals(X,geom)
+    nx = normals[:,0:1]
+    ny = normals[:,1:2]
+
+    Tt = -Tx*ny+ Ty*nx
+
+    return Tt
+
+# Karush-Kuhn-Tucker conditions for frictionless contact
+# gn>=0, Pn<=0, Tt=0 and gn.Pn=0  
+
+def positive_normal_gap(x, y, X):
+    '''
+    Enforces normal gap (gn) to be positive.
+    '''
+    gn = calculate_gap_in_normal_direction(x, y, X)
+
+    # If gn is negative, it will create contributions to overall loss. Aims is to get positive gap
+    return (1.0-tf.math.sign(gn))*gn
+
+def negative_normal_traction(x,y,X):
+    '''
+    Enforces contact pressure (Pn) to be positive.
+    '''
+    Pn = calculate_normal_traction(x,y,X)
+
+    # If Pn is positive, it will create contributions to overall loss. Aims is to get negative normal traction
+    return (1.0+tf.math.sign(Pn))*Pn
+
+def zero_tangential_traction(x,y,X):
+    '''
+    Enforces tangential part of contact traction (Tt) to be zero.
+    '''
+    Tt = calculate_tangential_traction(x,y,X)
+
+    return Tt
+
+def zero_complimentary(x,y,X):
+    '''
+    Enforces complimentary term to be zero.
+    '''
+    Pn = calculate_normal_traction(x,y,X)
+    gn = calculate_gap_in_normal_direction(x, y, X)
+
+    return gn*Pn
 
 # def boundary_circle(x, on_boundary):
 #     return on_boundary and np.isclose(np.linalg.norm(x - center, axis=-1), radius)
@@ -151,32 +182,39 @@ def boundary_circle_not_contact(x, on_boundary):
 def boundary_circle_contact(x, on_boundary):
     return on_boundary and np.isclose(np.linalg.norm(x - center, axis=-1), radius) and (x[0]>=x_loc_partition)
 
-ext_data = geom.random_boundary_points(1) #np.isclose(callback_pts[:,1],0)
-ext_data = ext_data[np.isclose(np.linalg.norm(ext_data - center, axis=-1), radius)]#,callback_pts[:,0]>=x_loc_partition)
-ext_data = ext_data[np.isclose(ext_data[:,1],-radius)]
-observe_u  = dde.PointSetBC(ext_data, np.zeros(1).reshape(-1,1), component=1)
+# Neumann BC
+bc_zero_traction_x = dde.OperatorBC(geom, calculate_traction_x, boundary_circle_not_contact)
+bc_zero_traction_y = dde.OperatorBC(geom, calculate_traction_y, boundary_circle_not_contact)
 
-bc1 = dde.OperatorBC(geom, zero_traction_x, boundary_circle_not_contact)
-bc2 = dde.OperatorBC(geom, zero_traction_y, boundary_circle_not_contact)
-
-bc3 = dde.OperatorBC(geom, fun_gap, boundary_circle_contact)
-bc4 = dde.OperatorBC(geom, normal_traction, boundary_circle_contact)
-bc5 = dde.OperatorBC(geom, tangential_traction, boundary_circle_contact)
-bc6 = dde.OperatorBC(geom, complimentary, boundary_circle_contact)
+# Contact BC
+bc_positive_normal_gap = dde.OperatorBC(geom, positive_normal_gap, boundary_circle_contact)
+bc_negative_normal_traction = dde.OperatorBC(geom, negative_normal_traction, boundary_circle_contact)
+bc_zero_tangential_traction = dde.OperatorBC(geom, zero_tangential_traction, boundary_circle_contact)
+bc_zero_complimentary = dde.OperatorBC(geom, zero_complimentary, boundary_circle_contact)
 
 n_dummy = 1
 data = dde.data.PDE(
     geom,
     pde_mixed_plane_strain,
-    [bc1,bc2,bc3,bc4,bc5,bc6,observe_u], #,bc3,bc4,bc5,bc6, observe_u
+    [bc_zero_traction_x,bc_zero_traction_y,bc_positive_normal_gap,bc_negative_normal_traction,bc_zero_tangential_traction,bc_zero_complimentary],
     num_domain=n_dummy,
     num_boundary=n_dummy,
     num_test=n_dummy,
-    train_distribution = "Sobol",
-    anchors=ext_data
+    train_distribution = "Sobol"
 )
 
 def output_transform(x, y):
+    '''
+    Hard BCs:
+    
+        Dirichlet terms
+            u(x=0)=0
+        
+        Neumann terms:
+            sigma_yy(y=0) = -ext_traction
+            sigma_xy(x=0) = 0
+            sigma_xy(y=0) = 0
+    '''
     u = y[:, 0:1]
     v = y[:, 1:2]
     sigma_xx = y[:, 2:3]
@@ -186,7 +224,7 @@ def output_transform(x, y):
     y_loc = x[:, 1:2]
     
     #return tf.concat([u*(-x_loc), ext_dips + v*(-y_loc), sigma_xx, sigma_yy, sigma_xy*(x_loc)*(y_loc)], axis=1)
-    return tf.concat([u*(-x_loc)/e_modul_analy, v/e_modul_analy, sigma_xx, ext_traction + sigma_yy*(-y_loc),sigma_xy*(x_loc)*(y_loc)], axis=1)
+    return tf.concat([u*(-x_loc)/e_modul, v/e_modul, sigma_xx, ext_traction + sigma_yy*(-y_loc),sigma_xy*(x_loc)*(y_loc)], axis=1)
 
 # two inputs x and y, output is ux and uy
 layer_size = [2] + [50] * 5 + [5]
@@ -195,7 +233,17 @@ initializer = "Glorot uniform"
 net = dde.maps.FNN(layer_size, activation, initializer)
 net.apply_output_transform(output_transform)
 
-loss_weights = [1e0,1e0,1e0,1e0,1e0,1e0,1e0,1e4,1e0,1e0,1e2,1e0]
+# weights due to PDE
+w_pde_1,w_pde_2,w_pde_3,w_pde_4,w_pde_5 = 1e0,1e0,1e0,1e0,1e0
+# weights due to Neumann BC
+w_zero_traction_x, w_zero_traction_y = 1e0,1e0
+# weights due to Contact BC
+w_positive_normal_gap = 1e4
+w_negative_normal_traction = 1e0
+w_zero_tangential_traction = 1e0
+w_zero_complimentary = 1e2
+
+loss_weights = [w_pde_1,w_pde_2,w_pde_3,w_pde_4,w_pde_5,w_zero_traction_x,w_zero_traction_y,w_positive_normal_gap,w_negative_normal_traction,w_zero_tangential_traction,w_zero_complimentary]
 
 model = dde.Model(data, net)
 model.compile("adam", lr=0.001, loss_weights=loss_weights)
@@ -208,32 +256,73 @@ losshistory, train_state = model.train(display_every=200)
 ############################## VISUALIZATION PARTS ################################
 ###################################################################################
 
-Quarter_Disc = QuarterDisc(radius=radius, center=center, mesh_size=0.005, angle=None, refine_times=None, gmsh_options=gmsh_options)
+fem_path = "Hertzian_fem/Hertzian_fem_fine_mesh.csv"
+df = pd.read_csv(fem_path)
+fem_results = df[["Points_0","Points_1","displacement_0","displacement_1","nodal_cauchy_stresses_xyz_0","nodal_cauchy_stresses_xyz_1","nodal_cauchy_stresses_xyz_3"]]
+fem_results = fem_results.to_numpy()
+node_coords_xy = fem_results[:,0:2]
+displacement_fem = fem_results[:,2:4]
+stress_fem = fem_results[:,4:7]
 
-gmsh_model, x_loc_partition, y_loc_partition = Quarter_Disc.generateGmshModel(visualize_mesh=False)
-
-revert_curve_list = []
-revert_normal_dir_list = [1,2,2,1]
-geom = GmshGeometry2D(gmsh_model, revert_curve_list=revert_curve_list, revert_normal_dir_list=revert_normal_dir_list)
-elasticity_utils.geom = geom
-
-X, offset, cell_types, dol_triangles = geom.get_mesh()
-
-output = model.predict(X)
-u_pred, v_pred = output[:,0], output[:,1]
-sigma_xx, sigma_yy, sigma_xy = output[:,2:3], output[:,3:4], output[:,4:5]
-sigma_rr, sigma_theta, sigma_rtheta = polar_transformation_2d(sigma_xx, sigma_yy, sigma_xy, X)
-
-
-combined_disp = tuple(np.vstack((np.array(u_pred.tolist()),np.array(v_pred.tolist()),np.zeros(u_pred.shape[0]))))
-combined_stress = tuple(np.vstack((np.array(sigma_xx.flatten().tolist()),np.array(sigma_yy.flatten().tolist()),np.array(sigma_xy.flatten().tolist()))))
-combined_stress_polar = tuple(np.vstack((np.array(sigma_rr.tolist()),np.array(sigma_theta.tolist()),np.array(sigma_rtheta.tolist()))))
-
-file_path = os.path.join(os.getcwd(), "Hertzian_normal_contact")
-
+X = node_coords_xy
 x = X[:,0].flatten()
 y = X[:,1].flatten()
 z = np.zeros(y.shape)
+triangles = tri.Triangulation(x, y)
+
+# predictions
+start_time_calc = time.time()
+output = model.predict(X)
+end_time_calc = time.time()
+final_time = f'Prediction time: {(end_time_calc - start_time_calc):.3f} seconds'
+print(final_time)
+
+u_pred, v_pred = output[:,0], output[:,1]
+sigma_xx_pred, sigma_yy_pred, sigma_xy_pred = output[:,2:3], output[:,3:4], output[:,4:5]
+sigma_rr_pred, sigma_theta_pred, sigma_rtheta_pred = polar_transformation_2d(sigma_xx_pred, sigma_yy_pred, sigma_xy_pred, X)
+
+combined_disp_pred = tuple(np.vstack((np.array(u_pred.tolist()),np.array(v_pred.tolist()),np.zeros(u_pred.shape[0]))))
+combined_stress_pred = tuple(np.vstack((np.array(sigma_xx_pred.flatten().tolist()),np.array(sigma_yy_pred.flatten().tolist()),np.array(sigma_xy_pred.flatten().tolist()))))
+combined_stress_polar_pred = tuple(np.vstack((np.array(sigma_rr_pred.tolist()),np.array(sigma_theta_pred.tolist()),np.array(sigma_rtheta_pred.tolist()))))
+
+# fem
+u_fem, v_fem = displacement_fem[:,0], displacement_fem[:,1]
+sigma_xx_fem, sigma_yy_fem, sigma_xy_fem = stress_fem[:,0:1], stress_fem[:,1:2], stress_fem[:,2:3]
+sigma_rr_fem, sigma_theta_fem, sigma_rtheta_fem = polar_transformation_2d(sigma_xx_fem, sigma_yy_fem, sigma_xy_fem, X)
+
+combined_disp_fem = tuple(np.vstack((np.array(u_fem.tolist()),np.array(v_fem.tolist()),np.zeros(u_fem.shape[0]))))
+combined_stress_fem = tuple(np.vstack((np.array(sigma_xx_fem.flatten().tolist()),np.array(sigma_yy_fem.flatten().tolist()),np.array(sigma_xy_fem.flatten().tolist()))))
+combined_stress_polar_fem = tuple(np.vstack((np.array(sigma_rr_fem.tolist()),np.array(sigma_theta_fem.tolist()),np.array(sigma_rtheta_fem.tolist()))))
+
+# error
+error_disp_x = abs(np.array(u_pred.tolist()) - u_fem.flatten())
+error_disp_y =  abs(np.array(v_pred.tolist()) - v_fem.flatten())
+combined_error_disp = tuple(np.vstack((error_disp_x, error_disp_y,np.zeros(error_disp_x.shape[0]))))
+
+error_stress_x = abs(np.array(sigma_xx_pred.flatten().tolist()) - sigma_xx_fem.flatten())
+error_stress_y =  abs(np.array(sigma_yy_pred.flatten().tolist()) - sigma_yy_fem.flatten())
+error_stress_xy =  abs(np.array(sigma_xy_pred.flatten().tolist()) - sigma_xy_fem.flatten())
+combined_error_stress = tuple(np.vstack((error_stress_x, error_stress_y, error_stress_xy)))
+
+error_polar_stress_x = abs(np.array(sigma_rr_pred.flatten().tolist()) - sigma_rr_fem.flatten())
+error_polar_stress_y =  abs(np.array(sigma_theta_pred.flatten().tolist()) - sigma_theta_fem.flatten())
+error_polar_stress_xy =  abs(np.array(sigma_rtheta_pred.flatten().tolist()) - sigma_rtheta_fem.flatten())
+combined_error_polar_stress = tuple(np.vstack((error_polar_stress_x, error_polar_stress_y, error_polar_stress_xy)))
+
+file_path = os.path.join(os.getcwd(), "Hertzian_fem_pinns_final_wo_anchors_2")
+
+dol_triangles = triangles.triangles
+offset = np.arange(3,dol_triangles.shape[0]*dol_triangles.shape[1]+1,dol_triangles.shape[1]).astype(dol_triangles.dtype)
+cell_types = np.ones(dol_triangles.shape[0])*5
 
 unstructuredGridToVTK(file_path, x, y, z, dol_triangles.flatten(), offset, 
-                      cell_types, pointData = { "displacement" : combined_disp,"stress" : combined_stress, "stress_polar": combined_stress_polar})
+                      cell_types, pointData = {"displacement_pred" : combined_disp_pred,
+                                               "displacement_fem" : combined_disp_fem,
+                                               "stress_pred" : combined_stress_pred, 
+                                               "stress_fem" : combined_stress_fem, 
+                                               "polar_stress_pred" : combined_stress_polar_pred, 
+                                               "polar_stress_fem" : combined_stress_polar_fem, 
+                                               "error_disp" : combined_error_disp, 
+                                               "error_stress" : combined_error_stress, 
+                                               "error_polar_stress" : combined_error_polar_stress
+                                            })
