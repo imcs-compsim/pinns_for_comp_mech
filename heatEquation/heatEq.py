c5689e79560a51b62c2af2745bced97506c37aeb
Otto <otto.kappen@unibw.de>
2023-10-20 14:05:53 +0200

    add and remove clear lines

--- heatEquation/heatEq.py
+++ heatEquation/heatEq.py
@@ -15,16 +15,18 @@ import numpy as np
 import deepxde as dde
 from deepxde.backend import torch
 
+
 def pde(x, y):
     """
     Expresses the PDE residual of the heat equation.
     """
-    # Diffusion coefficient 
+    # Diffusion coefficient
     k = 0.1
-    
+
     dy_t = dde.grad.jacobian(y, x, i=0, j=1)
     dy_xx = dde.grad.hessian(y, x, i=0, j=0)
     return dy_t - k * dy_xx
+
 
 # Initial condition
 def initial_condition(x):
@@ -36,8 +38,9 @@ def initial_condition(x):
     x : x passed to this function by the dde.pde is the NN input. Therefore,
         we must first extract the space coordinate.
     """
-    x_s = torch.tensor(x[:,0:1])
-    return torch.cos(np.pi*x_s)
+    x_s = torch.tensor(x[:, 0:1])
+    return torch.cos(np.pi * x_s)
+
 
 # Boundary condition
 def boundary_condition(x):
@@ -49,10 +52,10 @@ def boundary_condition(x):
     x : x passed to this function by the dde.pde is the NN input. Therefore,
         we must first extract the time coordinate.
     """
-    x_t = torch.tensor(x[:,1:2])
+    x_t = torch.tensor(x[:, 1:2])
     k = 0.1
-    
-    return -torch.exp(-k*(np.pi)**2*x_t)
+
+    return -torch.exp(-k * (np.pi) ** 2 * x_t)
 
 
 # Analytical solution
@@ -68,19 +71,21 @@ def analytical_solution(x, t, k):
     k : diffusion coefficient
     """
 
-    return np.exp(-k*np.pi**2*t) * np.cos(np.pi*x)
+    return np.exp(-k * np.pi**2 * t) * np.cos(np.pi * x)
+
 
 def postProcess(model):
-    '''
+    """
     Performs heat equation specific post-processing of a trained model.
 
     Parameters
     ----------
     X : trained deepxde model
 
-    '''
+    """
     import os, sys
     from pathlib import Path
+
     path_utils = str(Path(__file__).parent.parent.absolute()) + "/utils"
     sys.path.append(path_utils)
     from utils.postprocess.export_vtk import meshGeometry, solutionFieldOnMeshToVtk
@@ -91,9 +96,9 @@ def postProcess(model):
 
     temperature = model.predict(X)
 
-    pointData = { "temperature" : temperature.flatten()}
+    pointData = {"temperature": temperature.flatten()}
 
-    file_path = os.path.join(os.getcwd(),"heatEquation2D")
+    file_path = os.path.join(os.getcwd(), "heatEquation2D")
 
     solutionFieldOnMeshToVtk(X, triangles, pointData, file_path)
 
@@ -108,27 +113,36 @@ spaceDomain = dde.geometry.Interval(xmin, xmax)
 timeDomain = dde.geometry.TimeDomain(tmin, tmax)
 spaceTimeDomain = dde.geometry.GeometryXTime(spaceDomain, timeDomain)
 
+
 # Why do we define these functions. TimePDE seems to provide alreaddy a
 # boolean that indicates whether a point is on the boundary.
 def boundary_space(x, on_boundary):
     return on_boundary
 
+
 def boundary_initial(x, on_initial):
     return on_initial
 
+
 # Boundary and initial conditions
 bc = dde.DirichletBC(spaceTimeDomain, boundary_condition, boundary_space)
-ic = dde.IC(spaceTimeDomain, initial_condition , boundary_initial)
+ic = dde.IC(spaceTimeDomain, initial_condition, boundary_initial)
 
 # First guess on some scaling of the individual terms in the loss function
 # ToDo: Can we derive a physics-informed scaling of these terms?
 lw = [1, 100, 100]
 
 # Define the PDE problem and configurations of the network:
-data = dde.data.TimePDE(spaceTimeDomain, pde, [bc, ic], num_domain=250,
-                        num_boundary=32, num_initial=16, num_test=254,
-                        # auxiliary_var_function=diffusionCoeff
-                        )
+data = dde.data.TimePDE(
+    spaceTimeDomain,
+    pde,
+    [bc, ic],
+    num_domain=250,
+    num_boundary=32,
+    num_initial=16,
+    num_test=254,
+    # auxiliary_var_function=diffusionCoeff
+)
 
 net = dde.nn.FNN([2] + [20] * 3 + [1], "tanh", "Glorot normal")
 model = dde.Model(data, net)
@@ -167,16 +181,16 @@ X = np.vstack((np.ravel(xx), np.ravel(tt))).T
 # Compute and plot the exact solution for these query points
 k = 0.1
 usol = analytical_solution(xx, tt, k)
-plt.scatter(xx,tt,c=usol)
+plt.scatter(xx, tt, c=usol)
 plt.show()
 
 # Plot model prediction.
 y_pred = model.predict(X).reshape(t_dim, x_dim)
-plt.scatter(xx,tt,c=y_pred)
-plt.xlabel('x')
-plt.ylabel('t')
+plt.scatter(xx, tt, c=y_pred)
+plt.xlabel("x")
+plt.ylabel("t")
 ax = plt.gca()
-ax.set_aspect('equal','box')
-#plt.colorbar(cax=ax)
-plt.savefig('heatEqPred.pdf')
+ax.set_aspect("equal", "box")
+# plt.colorbar(cax=ax)
+plt.savefig("heatEqPred.pdf")
 plt.show()
