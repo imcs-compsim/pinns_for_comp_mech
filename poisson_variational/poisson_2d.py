04989f433af87fdcd1eb07a0170cbddaca07f609
Tarik Sahin <tarik.sahin@unibw.de>
2024-01-18 16:01:44 +0100

    Format example for vector form of weak formulation

--- poisson_variational/poisson_2d.py
+++ poisson_variational/poisson_2d.py
@@ -35,7 +35,7 @@ coord_left_corner=[-1,-1]
 coord_right_corner=[1,1]
 
 # create a block
-block_2d = Block_2D(coord_left_corner=coord_left_corner, coord_right_corner=coord_right_corner, mesh_size=0.2, gmsh_options=gmsh_options) #0.5
+block_2d = Block_2D(coord_left_corner=coord_left_corner, coord_right_corner=coord_right_corner, mesh_size=0.2, gmsh_options=gmsh_options)
 
 quad_rule = GaussQuadratureRule(rule_name="gauss_labotto", dimension=2, ngp=3) # gauss_legendre gauss_labotto
 coord_quadrature, weight_quadrature = quad_rule.generate()
@@ -61,34 +61,25 @@ geom = GmshGeometryElement(gmsh_model,
                            revert_normal_dir_list=revert_normal_dir_list)
 
 
-
 def pde(x, y):
     dy_xx = dde.grad.hessian(y, x)
     # Use tf.sin for backend tensorflow.compat.v1 or tensorflow
     x = deepxde_utils.to_numpy(x) #convert to numpy
     
     return -dy_xx  #- np.pi ** 2 * torch.sin(np.pi * x)
-    # Use torch.sin for backend pytorch
-    # return -dy_xx - np.pi ** 2 * torch.sin(np.pi * x)
-    # Use paddle.sin for backend paddle
-    # return -dy_xx - np.pi ** 2 * paddle.sin(np.pi * x)
+
+def weak_form(inputs, outputs, beg, n_e, n_gp, g_jacobian, g_weights, g_test_function, g_test_function_derivative):
+    du_x = dde.grad.jacobian(outputs, inputs, i=0, j=0)[beg:]
+    du_y = dde.grad.jacobian(outputs, inputs, i=0, j=1)[beg:]
     
-def weak_form(inputs, outputs, beg, end, e_jacobian, e_weights, e_test_function, e_test_function_derivative):
-    du_x = dde.grad.jacobian(outputs, inputs, i=0, j=0)[beg:end]
-    du_y = dde.grad.jacobian(outputs, inputs, i=0, j=1)[beg:end]
+    vx_x = g_test_function_derivative[:,0:1]
+    vy_y = g_test_function_derivative[:,1:2]
     
-    vx_x = e_test_function_derivative[:,0:1]
-    vy_y = e_test_function_derivative[:,1:2]
+    vx = g_test_function[:,0:1]
+    vy = g_test_function[:,1:2]
     
-    vx = e_test_function[:,0:1]
-    vy = e_test_function[:,1:2]
-    
-    # Use tf.sin for backend tensorflow.compat.v1 or tensorflow
-    return -e_weights[:,0:1]*e_weights[:,1:2]*(du_x*vx_x*vy + du_y*vy_y*vx)*e_jacobian
-    # Use torch.sin for backend pytorch
-    # return -dy_xx - np.pi ** 2 * torch.sin(np.pi * x)
-    # Use paddle.sin for backend paddle
-    # return -dy_xx - np.pi ** 2 * paddle.sin(np.pi * x)
+    weighted_residual = -g_weights[:,0:1]*g_weights[:,1:2]*(du_x*vx_x*vy + du_y*vy_y*vx)*g_jacobian
+    return bkd.reshape(weighted_residual, (n_e, n_gp))
 
 def boundary(x, on_boundary):
     return on_boundary
@@ -122,10 +113,13 @@ net = dde.nn.FNN(layer_size, activation, initializer)
 
 model = dde.Model(data, net)
 
-model.compile("adam", lr=0.001, metrics=["l2 relative error"])
+def mean_squared_error(y_true, y_pred):
+    return bkd.mean(bkd.square(y_true - y_pred), dim=0)
+
+model.compile("adam", lr=0.001, loss=mean_squared_error, metrics=["l2 relative error"])
 losshistory, train_state = model.train(iterations=10000)
 
-model.compile("L-BFGS")
+model.compile("L-BFGS", loss=mean_squared_error)
 losshistory, train_state = model.train(display_every=200)
 
 ################ Post-processing ################
@@ -155,11 +149,11 @@ combined_disp_act = tuple(np.vstack((u_act.flatten(), np.zeros(u_act.shape[0]), 
 combined_error = tuple(np.vstack((error.flatten(), np.zeros(error.shape[0]), np.zeros(error.shape[0]))))
 
 
-file_path = os.path.join(os.getcwd(), "2D_poisson_2")
+file_path = os.path.join(os.getcwd(), "2D_poisson_variational")
 
 x = X[:,0].flatten()
 y = X[:,1].flatten()
-z = np.zeros(y.shape)
+z = np.zeros_like(y)
 
 unstructuredGridToVTK(file_path, x, y, z, elements.flatten(), offset, 
                       cell_types, pointData = {"disp_pred" : combined_disp_pred, 
