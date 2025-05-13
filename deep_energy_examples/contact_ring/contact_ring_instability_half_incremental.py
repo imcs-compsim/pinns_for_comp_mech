2294af9d3d61d5a7326f83f94135f121b84adedc
Tarik Sahin <tarik.sahin@unibw.de>
2025-05-13 12:21:01 +0200

    Fix incremental contact example

--- deep_energy_examples/contact_ring/contact_ring_instability_half_incremental.py
+++ deep_energy_examples/contact_ring/contact_ring_instability_half_incremental.py
@@ -20,6 +20,8 @@ from utils.hyperelasticity import hyperelasticity_utils
 from utils.contact_mech.contact_utils import calculate_gap_in_normal_direction_deep_energy
 from utils.contact_mech import contact_utils
 
+from utils.postprocess.custom_callbacks import EpochTracker, SaveModelVTU
+
 from deepxde import backend as bkd
 
 from utils.deep_energy.deep_pde import DeepEnergyPDE
@@ -68,7 +70,7 @@ hyperelasticity_utils.nu = 0.3
 hyperelasticity_utils.stress_state = "plane_strain"
 nu,lame,shear,e_modul = compute_elastic_properties()
 
-applied_disp_y = -0.5
+applied_disp_y = -0.8
 
 # zero neumann BC functions need the geom variable to be 
 elasticity_utils.geom = geom
@@ -127,14 +129,32 @@ data = DeepEnergyPDE(
     num_test=None
 )
 
+# stabilzation model epoch
+stabilization_model_epoch = None
+epochs = 50000
+steps = 10
+
 def output_transform(x, y):
     u = y[:, 0:1]
     v = y[:, 1:2]
 
     x_loc = x[:, 0:1]
     y_loc = x[:, 1:2]
+    
+    if model.data.current_epoch is not None:
+        if stabilization_model_epoch is not None:
+            current_epoch = model.data.current_epoch - stabilization_model_epoch
+        else:
+            current_epoch = model.data.current_epoch
+
+        step_size = epochs / steps  # e.g., 10
+        current_step = int(current_epoch // step_size)
+        displacement_chunk = (current_step + 1) * applied_disp_y / steps
+    else:
+        displacement_chunk = 0
+        
     # tf.math.abs(x_loc)
-    return bkd.concat([u*(-y_loc)/e_modul, v*(-y_loc)/e_modul+applied_disp_y], axis=1)
+    return bkd.concat([u*(-y_loc)/e_modul, v*(-y_loc)/e_modul+displacement_chunk], axis=1)
 
 # two inputs x and y, output is ux and uy
 layer_size = [2] + [50] * 5 + [2]
@@ -145,17 +165,85 @@ net.apply_output_transform(output_transform)
 
 model = dde.Model(data, net)
 # if we want to save the model, we use "model_save_path=model_path" during training, if we want to load trained model, we use "model_restore_path=return_restore_path(model_path, num_epochs)"
+file_path = os.path.join(os.getcwd(), "deep_energy_contact_ring_instability_half")
+epoch_tracker = EpochTracker()
+model_saver_incremental = SaveModelVTU(period=int(epochs/steps), filename=file_path)
+
 model.compile("adam", lr=0.001)
-losshistory, train_state = model.train(epochs=5000, display_every=100)
+losshistory, train_state = model.train(epochs=epochs, callbacks=[epoch_tracker, model_saver_incremental], display_every=100)
 
-model.compile("L-BFGS")
-model.train_step.optimizer_kwargs["options"]['maxiter']=1000
-losshistory, train_state = model.train(display_every=200)
+# # # post
 
-# # post
+# # X, offset, cell_types, dol_triangles = geom.get_mesh()
+# # nu,lame,shear,e_modul = problem_parameters()
+
+# # # start_time_calc = time.time()
+# # output = model.predict(X)
+# # # end_time_calc = time.time()
+# # # final_time = f'Prediction time: {(end_time_calc - start_time_calc):.3f} seconds'
+# # # print(final_time)
+
+# # u_x_pred, u_y_pred = output[:,0], output[:,1]
+# # u_pred, v_pred = output[:,0], output[:,1]
+# # sigma_xx, sigma_yy, sigma_xy = model.predict(X, operator=stress_plane_strain)
+
+
+# # combined_disp = tuple(np.vstack((u_x_pred, u_y_pred, np.zeros(u_x_pred.shape[0]))))
+# # combined_stress = tuple(np.vstack((sigma_xx.flatten(), sigma_yy.flatten(), sigma_xy.flatten())))
+
+# # file_path = os.path.join(os.getcwd(), "deep_energy_hertzian")
+
+# # x = X[:,0].flatten()
+# # y = X[:,1].flatten()
+# # z = np.zeros(y.shape)
+
+# # #np.savetxt("Lame_inverse_large", X=np.hstack((X,output[:,0:2])))
+
+# # unstructuredGridToVTK(file_path, x, y, z, dol_triangles.flatten(), offset, 
+# #                       cell_types, pointData = { "displacement" : combined_disp,"stress" : combined_stress})
+
+
+# ###################################################################################
+# ############################## VISUALIZATION PARTS ################################
+# ###################################################################################
+
+# def polar_transformation_2d_tensor(T_xx, T_yy, T_xy, T_yx, X):
+#     '''
+#     Transforms a general 2nd-order 2D tensor (not necessarily symmetric) from Cartesian to polar coordinates.
+
+#     Parameters
+#     ----------
+#     X : numpy array, shape (N, 2)
+#         Coordinates of points.
+#     T_xx, T_yy, T_xy, T_yx : numpy arrays
+#         Components of the tensor in Cartesian coordinates.
+
+#     Returns
+#     -------
+#     T_rr, T_rtheta, T_thetar, T_thetatheta : numpy arrays
+#         Components of the tensor in polar coordinates.
+#     '''
+
+#     theta = np.arctan2(X[:, 1], X[:, 0])  # in radians
+#     cos_theta = np.cos(theta)
+#     sin_theta = np.sin(theta)
+
+#     # Rotation matrix components
+#     Q11 = cos_theta.reshape(-1,1)
+#     Q12 = sin_theta.reshape(-1,1)
+#     Q21 = -sin_theta.reshape(-1,1)
+#     Q22 = cos_theta.reshape(-1,1)
+
+#     # Perform the transformation using Einstein summation convention
+#     # T'_ij = Q_ip Q_jq T_pq
+#     T_rr = Q11 * (Q11 * T_xx + Q12 * T_yx) + Q12 * (Q11 * T_xy + Q12 * T_yy)
+#     T_rtheta = Q11 * (Q21 * T_xx + Q22 * T_yx) + Q12 * (Q21 * T_xy + Q22 * T_yy)
+#     T_thetar = Q21 * (Q11 * T_xx + Q12 * T_yx) + Q22 * (Q11 * T_xy + Q12 * T_yy)
+#     T_thetatheta = Q21 * (Q21 * T_xx + Q22 * T_yx) + Q22 * (Q21 * T_xy + Q22 * T_yy)
+
+#     return T_rr.astype(np.float32), T_rtheta.astype(np.float32), T_thetar.astype(np.float32), T_thetatheta.astype(np.float32)
 
 # X, offset, cell_types, dol_triangles = geom.get_mesh()
-# nu,lame,shear,e_modul = problem_parameters()
 
 # # start_time_calc = time.time()
 # output = model.predict(X)
@@ -164,14 +252,12 @@ losshistory, train_state = model.train(display_every=200)
 # # print(final_time)
 
 # u_x_pred, u_y_pred = output[:,0], output[:,1]
-# u_pred, v_pred = output[:,0], output[:,1]
-# sigma_xx, sigma_yy, sigma_xy = model.predict(X, operator=stress_plane_strain)
-
+# sigma_xx, sigma_yy, sigma_xy, sigma_yx = model.predict(X, operator=cauchy_stress_2D)
 
 # combined_disp = tuple(np.vstack((u_x_pred, u_y_pred, np.zeros(u_x_pred.shape[0]))))
 # combined_stress = tuple(np.vstack((sigma_xx.flatten(), sigma_yy.flatten(), sigma_xy.flatten())))
 
-# file_path = os.path.join(os.getcwd(), "deep_energy_hertzian")
+# file_path = os.path.join(os.getcwd(), f"deep_energy_contact_ring_instability_half_p_{abs(applied_disp_y)}")
 
 # x = X[:,0].flatten()
 # y = X[:,1].flatten()
@@ -181,69 +267,3 @@ losshistory, train_state = model.train(display_every=200)
 
 # unstructuredGridToVTK(file_path, x, y, z, dol_triangles.flatten(), offset, 
 #                       cell_types, pointData = { "displacement" : combined_disp,"stress" : combined_stress})
-
-
-###################################################################################
-############################## VISUALIZATION PARTS ################################
-###################################################################################
-
-def polar_transformation_2d_tensor(T_xx, T_yy, T_xy, T_yx, X):
-    '''
-    Transforms a general 2nd-order 2D tensor (not necessarily symmetric) from Cartesian to polar coordinates.
-
-    Parameters
-    ----------
-    X : numpy array, shape (N, 2)
-        Coordinates of points.
-    T_xx, T_yy, T_xy, T_yx : numpy arrays
-        Components of the tensor in Cartesian coordinates.
-
-    Returns
-    -------
-    T_rr, T_rtheta, T_thetar, T_thetatheta : numpy arrays
-        Components of the tensor in polar coordinates.
-    '''
-
-    theta = np.arctan2(X[:, 1], X[:, 0])  # in radians
-    cos_theta = np.cos(theta)
-    sin_theta = np.sin(theta)
-
-    # Rotation matrix components
-    Q11 = cos_theta.reshape(-1,1)
-    Q12 = sin_theta.reshape(-1,1)
-    Q21 = -sin_theta.reshape(-1,1)
-    Q22 = cos_theta.reshape(-1,1)
-
-    # Perform the transformation using Einstein summation convention
-    # T'_ij = Q_ip Q_jq T_pq
-    T_rr = Q11 * (Q11 * T_xx + Q12 * T_yx) + Q12 * (Q11 * T_xy + Q12 * T_yy)
-    T_rtheta = Q11 * (Q21 * T_xx + Q22 * T_yx) + Q12 * (Q21 * T_xy + Q22 * T_yy)
-    T_thetar = Q21 * (Q11 * T_xx + Q12 * T_yx) + Q22 * (Q11 * T_xy + Q12 * T_yy)
-    T_thetatheta = Q21 * (Q21 * T_xx + Q22 * T_yx) + Q22 * (Q21 * T_xy + Q22 * T_yy)
-
-    return T_rr.astype(np.float32), T_rtheta.astype(np.float32), T_thetar.astype(np.float32), T_thetatheta.astype(np.float32)
-
-X, offset, cell_types, dol_triangles = geom.get_mesh()
-
-# start_time_calc = time.time()
-output = model.predict(X)
-# end_time_calc = time.time()
-# final_time = f'Prediction time: {(end_time_calc - start_time_calc):.3f} seconds'
-# print(final_time)
-
-u_x_pred, u_y_pred = output[:,0], output[:,1]
-sigma_xx, sigma_yy, sigma_xy, sigma_yx = model.predict(X, operator=cauchy_stress_2D)
-
-combined_disp = tuple(np.vstack((u_x_pred, u_y_pred, np.zeros(u_x_pred.shape[0]))))
-combined_stress = tuple(np.vstack((sigma_xx.flatten(), sigma_yy.flatten(), sigma_xy.flatten())))
-
-file_path = os.path.join(os.getcwd(), f"deep_energy_contact_ring_instability_half_p_{abs(applied_disp_y)}")
-
-x = X[:,0].flatten()
-y = X[:,1].flatten()
-z = np.zeros(y.shape)
-
-#np.savetxt("Lame_inverse_large", X=np.hstack((X,output[:,0:2])))
-
-unstructuredGridToVTK(file_path, x, y, z, dol_triangles.flatten(), offset, 
-                      cell_types, pointData = { "displacement" : combined_disp,"stress" : combined_stress})
