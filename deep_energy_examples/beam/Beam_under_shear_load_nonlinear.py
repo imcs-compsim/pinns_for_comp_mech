dca505b72eb8228bd1450d04c60432e82eb5f6d9
Tarik Sahin <tarik.sahin@unibw.de>
2025-08-06 22:09:14 +0200

    Modify 3d beam by saving the model

--- deep_energy_examples/beam/Beam_under_shear_load_nonlinear.py
+++ deep_energy_examples/beam/Beam_under_shear_load_nonlinear.py
@@ -9,14 +9,16 @@ from utils.elasticity.elasticity_utils import stress_plane_strain, stress_plane_
 from utils.geometry.geometry_utils import calculate_boundary_normals, polar_transformation_2d
 from utils.geometry.custom_geometry import GmshGeometryElement
 from utils.geometry.gmsh_models import QuarterCirclewithHole
+import pyvista as pv
 
 from utils.geometry.custom_geometry import GmshGeometryElementDeepEnergy
 from utils.geometry.gmsh_models import Block_2D
 
-from utils.hyperelasticity.hyperelasticity_utils import strain_energy_neo_hookean_2d, compute_elastic_properties, cauchy_stress_2D
+from utils.hyperelasticity.hyperelasticity_utils import strain_energy_neo_hookean_2d, compute_elastic_properties, cauchy_stress_2D, green_lagrange_strain_2D, deformation_gradient_2D
 from utils.hyperelasticity import hyperelasticity_utils
 from utils.elasticity import elasticity_utils
 from deepxde import backend as bkd
+from deepxde.optimizers.config import LBFGS_options
 
 from utils.deep_energy.deep_pde import DeepEnergyPDE
 
@@ -24,6 +26,7 @@ from utils.vpinns.quad_rule import GaussQuadratureRule
 
 
 from utils.postprocess.custom_callbacks import SaveModelVTU
+from utils.hyperelasticity.hyperelasticity_utils import cauchy_stress_2D, first_piola_stress_tensor_2D
 
 '''
 Solves a hollow quarter cylinder under internal pressure (Lame problem)
@@ -33,7 +36,8 @@ https://onlinelibrary.wiley.com/doi/epdf/10.1002/nme.6132
 
 @author: tsahin
 '''
-
+# Sharp corners or geometric discontinuities (like re-entrant corners or sudden boundary changes) can cause stress singularities
+# https://www.fidelisfea.com/post/stress-singularities-at-reentrant-corners-a-fundamental-problem-in-fea
 
 gmsh_options = {"General.Terminal":1, "Mesh.Algorithm": 11}
 block_2d = Block_2D(coord_left_corner=[0,-1], coord_right_corner=[20,1], mesh_size=0.2, gmsh_options=gmsh_options)
@@ -142,7 +146,7 @@ def output_transform(x, y):
 
     x_loc = x[:, 0:1]
 
-    return bkd.concat([u*x_loc/e_modul,v*x_loc/e_modul], axis=1)
+    return bkd.concat([u*x_loc/l_beam,v*x_loc/l_beam], axis=1)
 
 # two inputs x and y, output is ux and uy
 layer_size = [2] + [64] * 5 + [2]
@@ -152,45 +156,105 @@ net = dde.maps.FNN(layer_size, activation, initializer)
 net.apply_output_transform(output_transform)
 
 file_path = os.path.join(os.getcwd(), "Beam_under_shear_load_nonlinear")
-stabilization_model_epoch = 100
-model_saver = SaveModelVTU(op=cauchy_stress_2D, period=1000, stabilization_epoch=stabilization_model_epoch, filename=file_path)
+# stabilization_model_epoch = 100
+# model_saver = SaveModelVTU(op=cauchy_stress_2D, period=1000, stabilization_epoch=stabilization_model_epoch, filename=file_path)
 
 model = dde.Model(data, net)
 # if we want to save the model, we use "model_save_path=model_path" during training, if we want to load trained model, we use "model_restore_path=return_restore_path(model_path, num_epochs)"
 
-model.compile("adam", lr=0.001)
-losshistory, train_state = model.train(epochs=stabilization_model_epoch, display_every=100)
+restore_model = True
+model_path = str(Path(__file__).parent.parent)+f"/trained_models/beam/beam_nonlinear"
 
-apply_load = True
-
-model.compile("adam", lr=0.001)
-losshistory, train_state = model.train(epochs=12000, display_every=100)
-
-# model.compile("L-BFGS")
-# model.train()
+if not restore_model:
+    # model.compile("adam", lr=0.001)
+    # losshistory, train_state = model.train(epochs=stabilization_model_epoch, display_every=100)
+    
+    apply_load = True
+    
+    model.compile("adam", lr=0.001)
+    losshistory, train_state = model.train(epochs=5000, display_every=100)
+    # if you want to save the model, run the following
+    # losshistory, train_state = model.train(epochs=5000, display_every=100, model_save_path=model_path)
+    
+    # For pytorch
+    # LBFGS_options["iter_per_step"] = 1
+    # LBFGS_options["maxiter"] = 500
+    
+    LBFGS_options["maxiter"] = 1500
+    model.compile("L-BFGS")
+    losshistory, train_state = model.train(display_every=100)
+    # losshistory, train_state = model.train(display_every=100, model_save_path=model_path)
+    
+    dde.saveplot(losshistory, train_state, issave=True, isplot=False)
+else:
+    n_epochs = 5083 
+    model_restore_path = model_path + "-"+ str(n_epochs) + ".ckpt"
+    
+    model.compile("adam", lr=0.001)
+    model.restore(save_path=model_restore_path)
 
 ###################################################################################
 ############################## VISUALIZATION PARTS ################################
 ###################################################################################
 
-X, offset, cell_types, dol_triangles = geom.get_mesh()
+file_path =  str(Path(__file__).parent.parent)+f"/reference_results/beam/bending_beam_test-structure.pvd"
+save_file_path = os.path.join(os.getcwd(), "Beam_under_shear_load_nonlinear")
 
-displacement = model.predict(X)
-sigma_xx, sigma_yy, sigma_xy, sigma_yx = model.predict(X, operator=cauchy_stress_2D)
-sigma_rr, sigma_theta, sigma_rtheta = polar_transformation_2d(sigma_xx, sigma_yy, sigma_xy, X)
+# Convert the Path object to a string
+reader = pv.get_reader(file_path)
 
-combined_disp = tuple(np.vstack((np.array(displacement[:,0].tolist()),np.array(displacement[:,1].tolist()),np.zeros(displacement[:,0].shape[0]))))
-combined_stress = tuple(np.vstack((np.array(sigma_xx.flatten().tolist()),np.array(sigma_yy.flatten().tolist()),np.array(sigma_xy.flatten().tolist()))))
-combined_stress_polar = tuple(np.vstack((np.array(sigma_rr.tolist()),np.array(sigma_theta.tolist()),np.array(sigma_rtheta.tolist()))))
+reader.set_active_time_point(-1)
+data = reader.read()[0]
 
-file_path = os.path.join(os.getcwd(), "Beam_under_shear_load_nonlinear")
+# FEM geometry does not match with PINNs, it should be shifted 10 unit to the right
+data.points[:, 0] += 10
+X = data.points[:,0:2]
 
-x = X[:,0].flatten()
-y = X[:,1].flatten()
-z = np.zeros(y.shape)
+output = model.predict(X)
+sigma_xx, sigma_yy, sigma_xy, _ = model.predict(X, operator=cauchy_stress_2D)
+eps_xx, eps_yy, eps_xy = model.predict(X, operator=green_lagrange_strain_2D)
+f_xx, f_yy, f_yx, f_xy = model.predict(X, operator=deformation_gradient_2D)
 
-unstructuredGridToVTK(file_path, x, y, z, dol_triangles.flatten(), offset,
-                      cell_types, pointData = { "displacement" : combined_disp,"stress" : combined_stress, "stress_polar": combined_stress_polar})
+cauchy_stress = np.column_stack((sigma_xx, sigma_yy, sigma_xy))
+strain = np.column_stack((eps_xx, eps_yy, eps_xy))
+deformation_gradient = np.column_stack((f_xx, f_yy, f_yx, f_xy, np.zeros_like(f_xx), np.zeros_like(f_xx)))
+# first_piola_stress = np.column_stack((p_xx, p_yy, p_xy))
+displacement = np.column_stack((output[:,0:1], output[:,1:2], np.zeros_like(output[:,0:1])))
+
+data.point_data['pred_displacement'] = displacement
+data.point_data['pred_cauchy_stress'] = cauchy_stress
+data.point_data['pred_strain'] = strain
+data.point_data['deformation_gradient'] = deformation_gradient
+# data.point_data['pred_first_piola_stress'] = first_piola_stress
+
+disp_fem = data.point_data['displacement']
+stress_fem = data.point_data['nodal_cauchy_stresses_xyz']
+
+error_disp = abs((disp_fem - displacement[:,0:2]))
+data.point_data['pointwise_displacement_error'] = error_disp
+# select xx, yy, and xy component (1st, 2nd and 4th column)
+columns = [0,1,3]
+error_stress = abs((stress_fem[:,columns] - cauchy_stress))
+data.point_data['pointwise_cauchystress_error'] = error_stress
+
+data.save(f"{save_file_path}.vtu")
+
+# X, offset, cell_types, dol_triangles = geom.get_mesh()
+
+# displacement = model.predict(X)
+# sigma_xx, sigma_yy, sigma_xy, sigma_yx = model.predict(X, operator=cauchy_stress_2D)
+
+# combined_disp = tuple(np.vstack((np.array(displacement[:,0].tolist()),np.array(displacement[:,1].tolist()),np.zeros(displacement[:,0].shape[0]))))
+# combined_stress = tuple(np.vstack((np.array(sigma_xx.flatten().tolist()),np.array(sigma_yy.flatten().tolist()),np.array(sigma_xy.flatten().tolist()))))
+
+# file_path = os.path.join(os.getcwd(), "Beam_under_shear_load_nonlinear")
+
+# x = X[:,0].flatten()
+# y = X[:,1].flatten()
+# z = np.zeros(y.shape)
+
+# unstructuredGridToVTK(file_path, x, y, z, dol_triangles.flatten(), offset,
+#                       cell_types, pointData = { "displacement" : combined_disp,"stress" : combined_stress})
 
 
 
