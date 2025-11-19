1fc19abaaadfa632addc873ca1a5f2b37c525cb8
Tarik Sahin <tsahin069@gmail.com>
2025-11-19 16:06:09 +0100

    Revert changes

--- examples/deep_energy_examples/3d_examples/3d_block_torsion_nonlinear_displacement.py
+++ examples/deep_energy_examples/3d_examples/3d_block_torsion_nonlinear_displacement.py
@@ -181,7 +181,7 @@ loss_weights=None
 
 model = dde.Model(data, net)
 
-restore_model = False
+restore_model = True
 model_path = str(Path(__file__).parent.parent.parent)+f"/pretrained_models/deep_energy_examples/3d_torsion/3d_torsion_nonlinear"
 
 if not restore_model:
