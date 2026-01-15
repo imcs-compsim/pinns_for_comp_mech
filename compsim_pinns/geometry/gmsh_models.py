75b18b53ec8e426801ae607ebcaba8e8dc97f3bb
Simon Völkl <simon.voelkl@unibw.de>
2026-01-15 09:03:13 +0100

    add new geometry class (square 2D block)

--- compsim_pinns/geometry/gmsh_models.py
+++ compsim_pinns/geometry/gmsh_models.py
@@ -603,232 +603,78 @@ class Block_2D(object):
                 gmsh.fltk.run()
 
         return gmsh_model
-    
-# class Block_3D_hex(object):
-#     def __init__(self, length, height, width, seed_l=10, seed_w=10, seed_h=10):
-#         self.length = length
-#         self.height = height
-#         self.width = width
-#         self.seed_l = seed_l
-#         self.seed_w = seed_w
-#         self.seed_h = seed_h
-    
-#     def generateGmshModel(self, visualize_mesh=False):
-#         '''
-#         Generates a 3D block with a structured hex mesh.
 
-#         Parameters
-#         ----------
-#         visualize_mesh : boolean
-#             a booelan value to show the mesh using Gmsh or not
-#         Returns 
-#         -------
-#         gmsh_model: Object
-#             gmsh model 
-#         '''
-#         # Parameters
-#         length= self.length #in x direction
-#         width = self.width #in y direction
-#         height = self.height #in z direction
+class Block_2D_square(object):
+    def __init__(self, coord_left_corner, coord_right_corner, mesh_size=0.25, gmsh_options=None):
+        self.coord_left_corner = coord_left_corner
+        self.coord_right_corner = coord_right_corner
+        self.mesh_size = mesh_size
+        self.gmsh_options = gmsh_options
+
+    def generateGmshModel(self, visualize_mesh=False):
+        '''
+        Generates a structured grid with elements with same side lengths.
+
+        Parameters
+        ----------
+        visualize_mesh : boolean
+            a booelan value to show the mesh using Gmsh or not
+        Returns 
+        -------
+        gmsh_model: Object
+            gmsh model 
+        '''
+
+        # Parameters
+        x0 = self.coord_left_corner[0]
+        y0 = self.coord_left_corner[1]
+        x1 = self.coord_right_corner[0]
+        y1 = self.coord_right_corner[1]
+        assert x1 > x0 and y1 > y0
+        l_x = x1 - x0
+        l_y = y1 - y0 
+        # Number of subdivisions
+        n_x, n_y = l_x / self.mesh_size, l_y / self.mesh_size
+        if (abs(n_x - round(n_x)) > 1E-9) or (abs(n_y - round(n_y)) > 1E-9):
+            raise ValueError(f"Mesh size leads to impossible mesh division.")
+        n_x, n_y = int(n_x), int(n_y)
         
-#         seed_l = self.seed_l
-#         seed_w = self.seed_w
-#         seed_h = self.seed_h
+        # Build rectangle with the built-in geometry kernel to control edges explicitly
+        gmsh.initialize(sys.argv)
+        gmsh_model = gmsh.model
+        geo = gmsh_model.geo
+        # Points
+        p1 = geo.addPoint(x0, y0, 0)
+        p2 = geo.addPoint(x1, y0, 0)
+        p3 = geo.addPoint(x1, y1, 0)
+        p4 = geo.addPoint(x0, y1, 0)
+        # Lines
+        l1 = geo.addLine(p1, p2)
+        l2 = geo.addLine(p2, p3)
+        l3 = geo.addLine(p3, p4)
+        l4 = geo.addLine(p4, p1)
+        # Curves
+        cl = geo.addCurveLoop([l1, l2, l3, l4])
+        s = geo.addPlaneSurface([cl])
+        # Synchronize before setting transfinite constraints
+        geo.synchronize()
+        # Transfinite lines: number of *nodes* along the curve. Use nx+1, ny+1.
+        gmsh_model.mesh.setTransfiniteCurve(l1, n_x + 1)
+        gmsh_model.mesh.setTransfiniteCurve(l3, n_x + 1)
+        gmsh_model.mesh.setTransfiniteCurve(l2, n_y + 1)
+        gmsh_model.mesh.setTransfiniteCurve(l4, n_y + 1)
+        # Structured surface + recombine to quads
+        gmsh_model.mesh.setTransfiniteSurface(s)
+        gmsh_model.mesh.setRecombine(2, s)
+        # Generate mesh
+        gmsh_model.mesh.generate()
         
-#         # create gmsh model instance
-#         gmsh_model = gmsh.model
+        # Visualize mesh
+        if visualize_mesh:
+            if '-nopopup' not in sys.argv:
+                gmsh.fltk.run()
 
-#         # initialize gmsh
-#         gmsh.initialize(sys.argv)
-        
-#         # Add points (corners of the cube)
-#         p1 = gmsh.model.geo.addPoint(0, 0, 0)
-#         p2 = gmsh.model.geo.addPoint(length, 0, 0)
-#         p3 = gmsh.model.geo.addPoint(length, width, 0)
-#         p4 = gmsh.model.geo.addPoint(0, width, 0)
-#         p5 = gmsh.model.geo.addPoint(0, 0, height)
-#         p6 = gmsh.model.geo.addPoint(length, 0, height)
-#         p7 = gmsh.model.geo.addPoint(length, width, height)
-#         p8 = gmsh.model.geo.addPoint(0, width, height)
-
-#         # Add lines (edges of the cube)
-#         l1 = gmsh.model.geo.addLine(p1, p2)
-#         l2 = gmsh.model.geo.addLine(p2, p3)
-#         l3 = gmsh.model.geo.addLine(p3, p4)
-#         l4 = gmsh.model.geo.addLine(p4, p1)
-
-#         l5 = gmsh.model.geo.addLine(p5, p6)
-#         l6 = gmsh.model.geo.addLine(p6, p7)
-#         l7 = gmsh.model.geo.addLine(p7, p8)
-#         l8 = gmsh.model.geo.addLine(p8, p5)
-
-#         l9 = gmsh.model.geo.addLine(p1, p5)
-#         l10 = gmsh.model.geo.addLine(p2, p6)
-#         l11 = gmsh.model.geo.addLine(p3, p7)
-#         l12 = gmsh.model.geo.addLine(p4, p8)
-
-#         # Define surfaces (6 faces of the cube)
-#         s1 = gmsh.model.geo.addPlaneSurface([gmsh.model.geo.addCurveLoop([-l1, -l4, -l3, -l2])]) #bottom
-#         s2 = gmsh.model.geo.addPlaneSurface([gmsh.model.geo.addCurveLoop([l5, l6, l7, l8])]) # top
-#         s3 = gmsh.model.geo.addPlaneSurface([gmsh.model.geo.addCurveLoop([l1, l10, -l5, -l9])]) #front
-#         s4 = gmsh.model.geo.addPlaneSurface([gmsh.model.geo.addCurveLoop([l2, l11, -l6, -l10])]) # right
-#         s5 = gmsh.model.geo.addPlaneSurface([gmsh.model.geo.addCurveLoop([l3, l12, -l7, -l11])]) # back
-#         s6 = gmsh.model.geo.addPlaneSurface([gmsh.model.geo.addCurveLoop([l4, l9, -l8, -l12])]) # left
-
-#         # Define the volume (body of the cube)
-#         gmsh.model.geo.addSurfaceLoop([s1, s2, s3, s4, s5, s6], 1)
-#         volume = gmsh.model.geo.addVolume([1])
-
-#         # Synchronize the geometry
-#         gmsh.model.geo.synchronize()
-
-#         # Set transfinite lines (structured mesh with hexahedral elements)
-#         # Define the number of divisions along each edge of the cube (e.g., 10 divisions)
-
-#         gmsh.model.mesh.setTransfiniteCurve(l1, seed_l)
-#         gmsh.model.mesh.setTransfiniteCurve(l2, seed_w)
-#         gmsh.model.mesh.setTransfiniteCurve(l3, seed_l)
-#         gmsh.model.mesh.setTransfiniteCurve(l4, seed_w)
-#         gmsh.model.mesh.setTransfiniteCurve(l5, seed_l)
-#         gmsh.model.mesh.setTransfiniteCurve(l6, seed_w)
-#         gmsh.model.mesh.setTransfiniteCurve(l7, seed_l)
-#         gmsh.model.mesh.setTransfiniteCurve(l8, seed_w)
-#         gmsh.model.mesh.setTransfiniteCurve(l9, seed_h)
-#         gmsh.model.mesh.setTransfiniteCurve(l10, seed_h)
-#         gmsh.model.mesh.setTransfiniteCurve(l11, seed_h)
-#         gmsh.model.mesh.setTransfiniteCurve(l12, seed_h)
-
-#         # Set transfinite surfaces for all 6 faces
-#         gmsh.model.mesh.setTransfiniteSurface(s1)
-#         gmsh.model.mesh.setTransfiniteSurface(s2)
-#         gmsh.model.mesh.setTransfiniteSurface(s3)
-#         gmsh.model.mesh.setTransfiniteSurface(s4)
-#         gmsh.model.mesh.setTransfiniteSurface(s5)
-#         gmsh.model.mesh.setTransfiniteSurface(s6)
-
-#         # Set transfinite volume (the entire cube)
-#         gmsh.model.mesh.setTransfiniteVolume(volume)
-
-#         # Set recombination to generate hexahedral elements
-#         gmsh.model.mesh.setRecombine(2, s1)
-#         gmsh.model.mesh.setRecombine(2, s2)
-#         gmsh.model.mesh.setRecombine(2, s3)
-#         gmsh.model.mesh.setRecombine(2, s4)
-#         gmsh.model.mesh.setRecombine(2, s5)
-#         gmsh.model.mesh.setRecombine(2, s6)
-        
-#         # Synchronize the geometry
-#         #gmsh_model.geo.synchronize()
-#         gmsh_model.occ.synchronize()
-
-#         # generate mesh
-#         gmsh_model.mesh.generate(3)
-
-#         if visualize_mesh:
-#             if '-nopopup' not in sys.argv:
-#                 gmsh.fltk.run()
-
-#         return gmsh_model
-    
-# class Block_3D_hex(object):
-#     def __init__(self, coord_left_corner, coord_right_corner, mesh_size=0.15, gmsh_options=None):
-#         self.coord_left_corner = coord_left_corner
-#         self.coord_right_corner = coord_right_corner
-#         self.mesh_size = mesh_size
-#         self.gmsh_options = gmsh_options
-
-#     def generateGmshModel(self, visualize_mesh=False):
-#         '''
-#         Generates a 3D block with structured hexahedral mesh.
-
-#         Parameters
-#         ----------
-#         visualize_mesh : boolean
-#             A boolean value to show the mesh using Gmsh or not
-#         Returns 
-#         -------
-#         gmsh_model: Object
-#             Gmsh model 
-#         '''
-
-#         # Parameters
-#         x0 = self.coord_left_corner[0]
-#         y0 = self.coord_left_corner[1]
-#         z0 = self.coord_left_corner[2]
-#         x1 = self.coord_right_corner[0]
-#         y1 = self.coord_right_corner[1]
-#         z1 = self.coord_right_corner[2]
-#         assert(x1 > x0)
-#         assert(y1 > y0)
-#         assert(z1 > z0)
-#         l = x1 - x0
-#         h = y1 - y0
-#         w = z1 - z0
-
-#         # Mesh size.
-#         lcar = self.mesh_size * min(h, l, w)
-
-#         # Create gmsh model instance
-#         gmsh_model = gmsh.model
-#         factory = gmsh_model.occ
-
-#         # Initialize gmsh
-#         gmsh.initialize(sys.argv)
-
-#         gmsh.option.setNumber("Mesh.CharacteristicLengthMin", lcar)
-#         gmsh.option.setNumber("Mesh.CharacteristicLengthMax", lcar)
-
-#         if self.gmsh_options:
-#             for command, value in self.gmsh_options.items():
-#                 if type(value).__name__ == 'str':
-#                     gmsh.option.setString(command, value)
-#                 else:
-#                     gmsh.option.setNumber(command, value)
-
-#         # Create box
-#         gmsh_model.add("Box")
-#         box = factory.addBox(x0, y0, z0, l, h, w)
-
-#         # Synchronize geometry
-#         gmsh_model.occ.synchronize()
-
-#         # Set transfinite lines to enforce structured mesh
-#         # Assuming that the geometry is a simple block
-#         volumes = gmsh_model.getEntities(3)
-#         surfaces = gmsh_model.getEntities(2)
-#         curves = gmsh_model.getEntities(1)
-
-#         # Apply transfinite meshing to all curves (edges of the box)
-#         for curve in curves:
-#             gmsh_model.mesh.setTransfiniteCurve(curve[1], 10)  # Adjust 10 for more refinement
-
-#         # Apply transfinite meshing to all surfaces
-#         for surface in surfaces:
-#             gmsh_model.mesh.setTransfiniteSurface(surface[1])
-
-#         # Apply transfinite meshing to the volume
-#         for volume in volumes:
-#             gmsh_model.mesh.setTransfiniteVolume(volume[1])
-
-#         # Recombine surfaces to convert triangular elements to quadrilateral
-#         for surface in surfaces:
-#             gmsh_model.mesh.setRecombine(2, surface[1])
-
-#         # Recombine the volume to create hexahedral elements
-#         for volume in volumes:
-#             gmsh_model.mesh.setRecombine(3, volume[1])
-
-#         # Synchronize geometry
-#         gmsh_model.occ.synchronize()
-
-#         # Generate mesh
-#         gmsh_model.mesh.generate(3)
-
-#         if visualize_mesh:
-#             if '-nopopup' not in sys.argv:
-#                 gmsh.fltk.run()
-
-#         return gmsh_model
+        return gmsh_model
 
 class SphereEighthHertzian(object):
     def __init__(self, radius=1.0, center=[0,0,0]):
@@ -866,7 +712,21 @@ class SphereEighthHertzian(object):
         gmsh.model.mesh.field.setNumber(2, "DistMin", 0.4 * self.radius)
         gmsh.model.mesh.field.setNumber(2, "DistMax", 0.9 * self.radius)
 
-        gmsh.model.mesh.field.setAsBackgroundMesh(2)
+        gmsh.model.mesh.field.add("Distance", 3)
+        gmsh.model.mesh.field.setNumbers(3, "CurvesList", [1])
+        gmsh.model.mesh.field.setNumber(3, "Sampling", 100)
+
+        gmsh.model.mesh.field.add("Threshold", 4)
+        gmsh.model.mesh.field.setNumber(4, "InField", 3)
+        gmsh.model.mesh.field.setNumber(4, "SizeMin", 0.05 * self.radius) 
+        gmsh.model.mesh.field.setNumber(4, "SizeMax", 0.1  * self.radius)
+        gmsh.model.mesh.field.setNumber(4, "DistMin", 0.2  * self.radius)
+        gmsh.model.mesh.field.setNumber(4, "DistMax", 0.6  * self.radius)
+
+        gmsh.model.mesh.field.add("Min", 5)
+        gmsh.model.mesh.field.setNumbers(5, "FieldsList", [2, 4])
+
+        gmsh.model.mesh.field.setAsBackgroundMesh(5)
 
         # Generate 3D mesh
         gmsh.model.mesh.generate(3)
